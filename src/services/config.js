097fd430e78b84e678b240cdf412050392e7216e
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2021-11-08 16:03:21 +0200

    Fix api query

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 const API_KEY = `?apikey=${process.env.REACT_APP_API_KEY}`;
 
-const API_URL = 'http://api.napster.com/v2.2/';
+const API_URL = 'https://api.napster.com/v2.2/';
 
 export {API_KEY, API_URL}
